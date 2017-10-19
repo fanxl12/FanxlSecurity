2d088018e2ebbddfcd96b1da9bf5911a498c3e33
fanxl12 <fxl421125>
2017-10-19 19:41:07 +0800

    1.实现微信登录;

--- fanxl-security-core/src/main/java/com/fanxl/security/core/properties/SocialProperties.java
+++ fanxl-security-core/src/main/java/com/fanxl/security/core/properties/SocialProperties.java
@@ -10,6 +10,8 @@ public class SocialProperties {
 
 	private QQProperties qq = new QQProperties();
 
+	private WeixinProperties weixin = new WeixinProperties();
+
 	public QQProperties getQq() {
         return qq;
 	}
@@ -24,5 +26,13 @@ public class SocialProperties {
 
 	public void setFilterProcessesUrl(String filterProcessesUrl) {
 		this.filterProcessesUrl = filterProcessesUrl;
+	}
+
+	public WeixinProperties getWeixin() {
+		return weixin;
+	}
+
+	public void setWeixin(WeixinProperties weixin) {
+		this.weixin = weixin;
 	}
 }
